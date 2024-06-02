aceb4e2c44d321d66b7b4a2d7227d76bf90240eb
Estrelaluci <167838542+Estrelaluci@users.noreply.github.com>
2024-06-02 15:16:33 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header('Jogando uma moeda')
+
+st.write('Ainda não é um aplicativo funcional. Em construção.')
+$ ls -a
+.git .gitignore .streamlit app.py README.md requirements.txt
+$ git add .
+$ git commit -am "initial commit"
+$ git push
